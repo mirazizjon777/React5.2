566feba7ea7da049f05385f0422433d9014d6c0e
mirazizjon777 <mirazizjonisroilov1@gmail.com>
2023-04-08 14:51:02 +0500

    sass

---
+++ 5.2/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return <Card nom="Mirazizjon"/>;
+};
+
+export default App;
+
+import React from "react";
+import Card from "./Card";
---
+++ 5.2/src/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (Ism) => {
+  return (
+    <div>
+      <h1>{Ism.nom}</h1>
+    </div>
+  );
+};
+
+export default Card;
